8d5096f3b699addd6231fb456e1ae67269f54015
Andrew Bartkus <github@bartkusa.com>
2015-07-06 23:58:11 -0700

    PCharsEditor: Add/Load/Save buttons
    Also, "newest" character appears at the top of the list.

--- js/ex3/components/PersistentCharactersEditor.jsx
+++ js/ex3/components/PersistentCharactersEditor.jsx
@@ -1,21 +1,30 @@
 "use strict";
 
-var React = require('react/addons');
-var PersistentCharacter = require('./PersistentCharacterEditor');
+const React = require('react/addons');
+const PersistCharEditor = require('./PersistentCharacterEditor');
+const PersistentCharacter = require('ex3/models/PersistentCharacter');
+const charActions = require('ex3/actions/CharActions');
 
 
-var PersistentCharactersEditor = module.exports = React.createClass({
+const PersistentCharactersEditor = module.exports = React.createClass({
 	render: function() {
 		return (
-			<div className="PersistentCharactersEditor">
-				<button className="addChar">Add</button>
+			<div className="PersistentCharactersEditor clearfix">
+				<h1>Persistent Characters</h1>
+				<div className="actions">
+					<button className="addChar btn btn-success" onClick={this._addOnClick}>Add</button>&nbsp;
+					<button className="addChar btn btn-default" onClick={this._loadOnClick}>Load</button>
+					<button className="addChar btn btn-default" onClick={this._saveOnClick}>Save</button>
+				</div>
 				{this._renderCharList()}
 			</div>
 		);
 	},
 
 	_renderCharList: function() {
-		let pcList = this.props.persistentCharacters;
+		const pcList = this.props.persistentCharacters
+				.map((x, i) => x)
+				.reverse();
 
 		if (!pcList || pcList.length <= 0) return (
 			<div className="ghostTown">You have no characters.</div>
@@ -27,8 +36,20 @@ var PersistentCharactersEditor = module.exports = React.createClass({
 	_renderChar: function(persistChar, index) {
 		return (
 			<li key={persistChar.id}>
-				<PersistentCharacter persistentCharacter={persistChar} />
+				<PersistCharEditor persistentCharacter={persistChar} />
 			</li>
 		);
+	},
+
+	_addOnClick: function() {
+		charActions.add(new PersistentCharacter());
+	},
+
+	_loadOnClick: function() {
+		charActions.load();
+	},
+
+	_saveOnClick: function() {
+		charActions.save();
 	}
 });
