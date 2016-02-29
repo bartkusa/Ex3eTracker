e1d3530b0cf47727a4d794b92e843dc7fcbaa669
Andrew Bartkus <github@bartkusa.com>
2016-02-28 17:06:08 -0800

    CharUtils: deprecate pluralize(), and add toIdSet()

--- js/ex3/stores/CharUtils.js
+++ js/ex3/stores/CharUtils.js
@@ -1,15 +1,24 @@
-export function pluralize(x) {
-	if (x == null) return [];
-	if (Array.isArray(x)) return x;
-	return [x];
-};
+import { asArray } from 'ex3/funcs/utils';
+
 
 export function toIds(things) {
-	return pluralize(things)
+	return asArray(things)
 			.map((thing) => thing.id >= 0 ? thing.id : thing);
+};
+
+export function toIdSet(things) {
+	return toIds(things)
+		.reduce(
+			(set, curId) => (set[curId] = true, set),
+			{}
+		);
 };
 
 export function idsMatch(cur, selector) {
 	if (!selector) return false;
 	return cur.id === (selector.id || selector);
+};
+
+export function pluralize(x) { //Deprecated
+	return asArray(x);
 };
