59a3f1af3388b93478598f33a047bf19acd5ac5e
Andrew Bartkus <github@bartkusa.com>
2016-03-18 21:26:35 -0700

    [bug] Truncate deltanums' precision, because scientific notation and CSS don't mix
    Safari webkit can't parse "translate3D( -1.456e-12px, ...)"

--- js/ex3/components/TouchKnob/DeltaNum.jsx
+++ js/ex3/components/TouchKnob/DeltaNum.jsx
@@ -19,7 +19,7 @@ export default ((p) => {
 	return (
 		<div className={`DeltaNum ${p.direction}`}
 				style={{
-					transform: `translate3D(${x}px, ${y}px, 0)`,
+					transform: `translate3D(${truncate(x)}px, ${truncate(y)}px, 0)`,
 					// opacity: Math.round(p.opacity * 100)/100, // weird, Chrome won't rerender this sometimes if opacity is applied
 				}}>
 			{p.delta >= 0 ? '+' : ''}
@@ -28,3 +28,7 @@ export default ((p) => {
 	);
 
 });
+
+function truncate(num) {
+	return Math.round(num * 10e4) / 10e4;
+};
